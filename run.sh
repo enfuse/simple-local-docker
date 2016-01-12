4d880ab8cdebc7ab2ab95e927c2fbca08ee1d7b5
Juanjo <juanjo.lopez@biko2.com>
2016-01-12 09:13:04 +0100

    First commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run -ti -h dev.local \
+           -v "$PWD/..":/var/www \
+           -v "$PWD/database":/var/lib/mysql \
+           -p 0.0.0.0:80:80 \
+           -p 0.0.0.0:443:443 \
+           -p 0.0.0.0:3306:3306 \
+           -p 0.0.0.0:9000:9000 \
+	   -p 0.0.0.0:8025:8025 \
+            drupaldev
