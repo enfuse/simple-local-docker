200ae6393fd6726481ed05d0aeec6db343dccec1
Juanjo <juanjo.lopez@biko2.com>
2016-01-13 10:58:40 +0100

    Ruteo ssh y subo memoria php

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker run -ti -h dev.local \
            -v "$PWD/..":/var/www \
            -v "$PWD/database":/var/lib/mysql \
            -p 0.0.0.0:80:80 \
+	   -p 0.0.0.0:2222:22 \
            -p 0.0.0.0:443:443 \
            -p 0.0.0.0:3306:3306 \
            -p 0.0.0.0:9000:9000 \
